8f1a1556e1f6f19580d8b9ccda08c67b97aaf1ae
AyDy Burling <aydy.you@gmail.com>
2019-11-19 19:37:13 -0600

    questions completed

--- index.js
+++ index.js
@@ -4,37 +4,85 @@
 center screen: welcome: introduction to the quiz, directions/parameters and a start button*/
 
 // on click of the start button, the button in the welcome section will disapear
-
-function startquiz () {}
-
-const startBtnEl = document.getElementById('gitLit').addEventListener('click', function () {
-	const vanishConEl = document.getElementById('welcome-instructions');
-	vanishConEl.setAttribute('class', 'container d-none');
-	console.log(startBtnEl);
-	// and the quesions will appear. answer choices to be dislplayed as buttons underneath the question. all within an array*/
-	for (i = 0; i < questions.length; i++) {
+let questionIndex= 0;
+let correct = 0;
+let wrong = 0;
+const whamDisplay = document.getElementById('wham');
+function startquiz () {
+    // for (i = 0; i < questions.length; i++) {
 		let options = [];
 		const questionDisplay = document.getElementById('q-and-a');
-		questionDisplay.innerHTML = Object.values(questions[i]['title']).join('');
-		const choicesDisplay = document.getElementById('choices');
-		for (x = 0; x < 4; x++) {
+        const choicesDisplay = document.getElementById('choices');
+        questionDisplay.innerHTML="";
+        choicesDisplay.innerHTML="";
+        whamDisplay.innerHTML="";
+		questionDisplay.innerHTML = Object.values(questions[questionIndex]['title']).join('');
+        for (x = 0; x < 4; x++) {
 			let newButton = document.createElement('button');
 			newButton.setAttribute('id', x);
 			options.push(newButton);
 			console.log(options);
 			choicesDisplay.append(options[x]);
-			newButton.innerHTML = Object.values(questions[i]['choices'][x]).join('');
-		}
-		document.getElementById('choices').addEventListener('click', function () {
-			console.log('I got clicked');
-		});
-	}
+            newButton.innerHTML = Object.values(questions[questionIndex]['choices'][x]).join('');
+            
+        }
+    // }
+}
+
+const startBtnEl = document.getElementById('gitLit').addEventListener('click', function () {
+	const vanishConEl = document.getElementById('welcome-instructions');
+	vanishConEl.setAttribute('class', 'container d-none');
+	console.log(startBtnEl);
+    startquiz();
+    // and the quesions will appear. answer choices to be dislplayed as buttons underneath the question. all within an array*/
+			
 });
 
-startquiz();
+document.getElementById('choices').addEventListener("click", function (event) {
+    // console.log(event.target.innerText);
+    // Google Fu: how to grab text on button vanilla JS
+//    console.log("current answer", questions[questionIndex].answer)
+   // on click of correct button: display "Correct!" beneath the question and next question will appear
 
-// on click of correct button: display "Correct!" beneath the question and next question will appear
+   // on click of incorrect button: display "Wrong!" beneath the question and next question will appear
+  
+   if (event.target.innerText===questions[questionIndex].answer){
+   console.log("correct")
+   whamDisplay.innerHTML= "Correct!"
+   correct++
+   }else{
+    whamDisplay.innerHTML= "Wrong!"  
+    wrong++
+    console.log("wrong")
+   }
+   questionIndex++
+   setTimeout(function(){
+    // console.log(correct, wrong)
+    console.log("questionIndex", questionIndex)
+    console.log("questions.length", questions.length)
+    if (questionIndex<questions.length){
+        startquiz ()
+    }else{
+       alert ("game over")
+    }
+      
+   },500);
 
-// on click of incorrect button: display "Wrong!" beneath the question and next question will appear
+
+});  
+
+// how to indicate when the game is done
+
+
+
+
+
+
+
+
+
+
+
+
 
 // last screen will show "All Done!" with winner score underneath and an input box for user to enter name: a submit button : a clear score button.
--- questions.js
+++ questions.js
@@ -1,7 +1,33 @@
+// Q&A courtesy of https://www.sanfoundry.com/1000-javascript-questions-answers/
 const questions = [
     {
-      title:"How tall is a giraffe?",
-      choices: ["5 ft", "8 ft", "10 ft", "15 ft"],
-      answer:3
-    }
+      title:"When does the function name become optional in JavaScript?",
+      choices: ["When the function is defined as a looping statement", "When the function is defined as expressions", " When the function is predefined", " when the function is called"],
+      answer:"When the function is defined as expressions"
+    },
+    {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+
+  {
+      title: "Which function among the following lets to register a function to be invoked once?",
+      choices: ["setTime", "setTotaltime", "setInterval", "settime"],
+      answer: "setTimeout"
+    }, 
+  
+    { 
+      title: "The type that specifies what kind of event occurred is ____________.",
+      choices: ["event type", "even target", "both event type and even taret", "interface"],
+      answer: "event type"
+    }, 
+  
+
 ]
